6dbbe68eaba08557e5e63dab54403de37b820920
Ido <idokestin@gmail.com>
2018-11-07 23:07:59 +0000

    changes

--- MissKeep App/js/pages/about-page.cmp.js
+++ MissKeep App/js/pages/about-page.cmp.js
@@ -2,7 +2,16 @@
 export default{
     name:'about-page',
 template:`
-    <h1>About keep</h1>
+    <section>
+    
+        <h1 class="about-title">About missKeep</h1>
+        <h2>
+            The MissKeep App is an application where you can edit your memories,
+            in the form of pictures, words and even important lists to manage
+            everyday tasks
+        </h2>
+        <img class="img-reminder" src="img/garfild.png" />
+    </section>
 
 `
 
--- MissKeep App/js/pages/home-page.cmp.js
+++ MissKeep App/js/pages/home-page.cmp.js
@@ -4,7 +4,9 @@ export default {
     template: `
     <section class="header-home">
         <h1 class="home-title">Welcome To MissKeep App</h1>
-        <img class="img-home" src="img/home.jpg" />
+        <img class="img-home" src="img/home.png" />
+        <h1 class="home-title">Sometimes you just have to relax and let us remind you</h1>
+
     </section>
     `
 }
