46c2066bd5fe170532433e6ba96bbbf33c68c02a
Ido <idokestin@gmail.com>
2018-11-07 16:08:23 +0000

    miss keep change 1

--- MissKeep App/js/pages/keep-app.cmp.js
+++ MissKeep App/js/pages/keep-app.cmp.js
@@ -1,32 +1,58 @@
-
+import {noteService} from '../service/note.service.js';
+import keepList from '../cmps/keep-list.cmp.js';
+import keepFilter from '../cmps/keep-filter.cmp.js';
 
 export default {
-    name:'keep-app',
+    name: 'keep-app',
     template: `
-        <section>
-            <h1>The Notes Area</h1>
-            <img class="img-header" src="img/logo.jpg">
-            
-           
+        <section class="flex column">
+            <div class="keep-head-container flex space-between">
+                <h1>Keep remember</h1>
+                <img class="img-header" src="img/logo.png">
+            </div>
+
+            <section>
+                <button class="btn-add">Add New Note</button>
+            </section>
+            <keep-filter></keep-filter>
+            <keep-list :notes="notesToShow"></keep-list>
+          
+            <section class="keep-footer">
+                <h6>cofferigths 2018</h6>
+            </section>
         </section>
     `,
-    // data() {
-       
-    // },
 
-    // created() {
-       
-    // },
-
-    computed: {
-     
+    data() {
+        return {
+            notes: null,
+            filter: null
+        }
     },
-    methods: {
+
+    created() {
+        noteService.query()
+        .then(res => {
+            this.notes = res
+        })
+    },
+
+
     
+    computed:{
+        notesToShow(){
+                if (!this.filter) return this.notes
+
+                return this.filter.icludes()
+            console.log(this.notes);
+            return this.notes
+        }
+    },
+    
+    components: {
+        keepList,
+        keepFilter
 
     },
-    components: {
-       
-    }
-}
 
+}
---
+++ MissKeep App/js/service/note.service.js
@@ -0,0 +1,71 @@
+import { storageService } from './storage.service.js'
+import { utilService } from './util.service.js'
+
+const Notes_KEY = 'books';
+
+var gNotes;
+
+var basiceNotes = [
+    {
+        title: 'buy shoes',
+        description: 'been there after met joshua in the park.. the green model',
+        location: '216 ben jehuda, shoes land',
+        created: '11/11/2018'
+    },
+    {
+        title: 'Order Tickets',
+        description: 'My yearly vecation',
+        location: 'fly fast.com',
+        created: '11/11/2018'
+    },
+]
+
+
+// this function retun promise of notes array  
+function query() {
+    gNotes = storageService.load(Notes_KEY);
+    if (!gNotes) {
+        gNotes = createNotes();
+        storageService.store(Notes_KEY, gNotes)
+    }
+    return Promise.resolve(gNotes)
+}
+
+// init by query function
+function createNotes() {
+    return basiceNotes;
+}
+
+
+// function addNote(noteToAdd) {
+//     gNotes.push(noteToAdd);
+//     storageService.store(Notes_KEY, gNotes)
+// }
+
+// function getNoteById(noteId) {
+//     var theBook = gNotes.find(note => note.id === noteId);
+//     return Promise.resolve(theBook)
+// }
+
+// function getNextNotekId(noteId) {
+//     const currNoteIdx = gNotes.findIndex(note =>noteId === note.id) 
+    
+//     const nextNote = gNotes[currNoteIdx + 1] ? gNotes[currNoteIdx + 1] : gNotes[0]
+//     return Promise.resolve(nextNote)
+// }
+
+
+// function getPrevNoteId(noteId){
+//     const currBookIdx = gNotes.findIndex(book =>noteId === book.id) 
+
+// const prevBook = gNotes[currBookIdx - 1] ? gNotes[currBookIdx - 1] : gNotes[gNotes.length-1]
+// return Promise.resolve(prevBook)
+// }
+
+export const noteService = {
+    query,
+    // getNoteById,
+    // addNote,
+    // getNextNotekId,
+    // getPrevNoteId
+}
