b0a4082d60a4c20316b1b1fa1ab5a88d0be60413
ron2man Ron Shtaiman <ron2man10@gmail.com>
2018-11-08 09:01:01 +0200

    commit to-be-sure-900

--- misterEmail/js/cmp/email-app-main.cmp.js
+++ misterEmail/js/cmp/email-app-main.cmp.js
@@ -9,17 +9,18 @@ export default {
     template: `
 <div  class="main-container">
         <header-comp></header-comp>
-        <controller-comp :unReadEmails="unReadEmails" @onInput="setSearchQuery" @setFilter="setFilter"></controller-comp>
+        <controller-comp :unReadEmails="unReadEmails" @onInput="setSearchQuery" @setFilter="setFilter" @setSort="setSort"></controller-comp>
         <email-list @unReadCount="updateUnRead" :emails="emailsToShow"></email-list>
         <footer-comp></footer-comp>
     </div>
 `, data() {
         return {
             unReadEmails: 0,
-            
-            emails:[],
+
+            emails: [],
             searchQuery: '',
-            filter: {all:true,read:false,unread:false}
+            filter: { all: true, read: false, unread: false },
+            sortBy: 'date',
         }
     },
     components: {
@@ -29,37 +30,51 @@ export default {
         footerComp
     },
     methods: {
-        setSearchQuery(query){
+        setSearchQuery(query) {
             this.searchQuery = query;
-            // console.log(query);
         },
         updateUnRead(unreadEmails) {
             this.unReadEmails = unreadEmails;
         },
-        setFilter(filter){
+        setFilter(filter) {
             this.filter = filter;
-            console.log(filter)
         },
-        countUnreadeEmails(){
-            this.unReadEmails = this.emails.filter(email=> email.isRead === false);
-            // this.unReadEmails = this.emails.filter(email=> !email.isRead)
-            
-            // console.log(this.unReadEmails.length)
+        setSort(sort) {
+            console.log('emit is here - ', sort)
+            this.sortBy = sort;
+        },
+        countUnreadeEmails() {
+            this.unReadEmails = this.emails.filter(email => email.isRead === false);
+            // this.unReadEmails = this.emails.filter(email=> !email.isRead)            
         }
     },
     computed: {
-        emailsToShow(){
+        emailsToShow() {
             // Filter By Search Query
-            return this.emails.filter(email => 
+            return this.emails.filter(email =>
                 email.subject.toUpperCase().includes(this.searchQuery.toUpperCase())
-             || email.body.toUpperCase().includes(this.searchQuery.toUpperCase())
-             || email.from.toUpperCase().includes(this.searchQuery.toUpperCase())).filter(email =>
-                (this.filter.all && email) || (this.filter.read && email.isRead) || (this.filter.unread && !email.isRead) 
-                );
-        } 
+                || email.body.toUpperCase().includes(this.searchQuery.toUpperCase())
+                || email.from.toUpperCase().includes(this.searchQuery.toUpperCase())).filter(email =>
+                    (this.filter.all && email) || (this.filter.read && email.isRead) || (this.filter.unread && !email.isRead))
+                .sort((this.sortBy === 'date' ? sortEmailsByDate : sortEmailsBySubject))
+
+            function sortEmailsBySubject(emailA, emailB) {
+                var subjectA = emailA.subject.toUpperCase();
+                var subjectB = emailB.subject.toUpperCase();
+                if (subjectA < subjectB) return -1;
+                if (subjectA > subjectB) return 1;
+                return 0;
+            }
+
+            function sortEmailsByDate(emailA, emailB) {
+                if (emailA.sentAt > emailB.sentAt) return -1;
+                if (emailA.sentAt < emailB.sentAt) return 1;
+                return 0;
+            }
+        }
     },
-    created(){
-        console.log('%c Email APP Main CMP Created','color:black;background-color:gold;font-size:1.2em');
+    created() {
+        console.log('%c Email APP Main CMP Created', 'color:black;background-color:gold;font-size:1.2em');
         this.emails = emailService.query();
     }
 
